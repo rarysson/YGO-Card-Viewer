7ec58dd28ac96742061581f89557cb710e74e73f
Rarysson Guilherme <r_guilherme12@hotmail.com>
2022-05-21 14:48:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# YGO Card Viewer
+
+A study about Next.js.
+
+A Yu-Gi-Oh! card viewer.
---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Yu-Gi-Oh! Card Viewer</title>
+        <link rel="icon" href="/favicon.webp" />
+      </Head>
+
+      <h1>YGO Card Viewer</h1>
+    </div>
+  )
+}
